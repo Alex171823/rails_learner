1a1f7f949f45ed67f5decb4ede6c823e9cff27d6
iverty <berezhnoy20025@gmail.com>
2022-06-22 19:23:42 +0300

    lesson10

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  resources :trains
   get 'welcome/index'
+
+  resources :railway_stations
+
+  # Defines the root path route ("/")
+  # root "articles#index"
   root 'welcome#index'
-  # root 'searches#new'
 
-  namespace :admin do
-    resources :tickets, except: %i[new]
-    resources :trains do
-      resources :wagons
-      # STI required
-      resources :sitting_wagons, controller: 'wagons', type: 'SittingWagon'
-      resources :economy_wagon, controller: 'wagons', type: 'EconomyWagon'
-      resources :sleeping_wagon, controller: 'wagons', type: 'SleepingWagon'
-      resources :coupe_wagons, controller: 'wagons', type: 'CoupeWagon'
-    end
-
-    resources :railway_stations do
-      patch :update_position, on: :member
-    end
-
-    resources :routes
-    resources :tickets
-  end
-
-  resources :tickets
-
-  # resolves search as singular resource
-  resource :search, only: %i[new show]
-  resolve('Search') { [:search] }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end

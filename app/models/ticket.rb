1c77df531fc8f9995f26ee440d2d8ef9330b311c
iverty <berezhnoy20025@gmail.com>
2022-06-28 11:48:33 +0300

    tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,20 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :train
   belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
-  belongs_to :user
-
-  after_create :buy_email_notification
-  after_destroy :delete_email_notification
-
-  validates :name, :passport, :user_id, presence: true
-
-  private
-
-  def buy_email_notification
-    TicketsMailer.buy_ticket(user, self).deliver_now
-  end
-
-  def delete_email_notification
-    TicketsMailer.delete_ticket(user, self).deliver_now
-  end
+  # belongs_to :user
 end

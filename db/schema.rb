c91e733ba501c62ac8e93ccaa794fb6a5f3d037f
iverty <berezhnoy20025@gmail.com>
2022-06-28 11:55:16 +0300

    user + tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_082609) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_085218) do
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_082609) do
     t.integer "end_station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|
@@ -45,6 +47,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_082609) do
     t.integer "route_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

be07af0bc1d2820521e7ee175d70cce1e3309205
iverty <berezhnoy20025@gmail.com>
2022-06-28 11:55:16 +0300

    user + tickets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  has_many :tickets, dependent: :destroy
+  has_many :tickets
 end

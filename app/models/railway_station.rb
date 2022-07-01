b62c33beed25be5da0150e076fd424d3b1636a09
iverty <berezhnoy20025@gmail.com>
2022-07-01 18:08:24 +0300

    done

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -6,5 +6,6 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
+  # default_scope { order(:index_number) }
   scope :ordered_by_index, -> { order(index_number: :asc) }
 end

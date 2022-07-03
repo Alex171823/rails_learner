b133dce4a490a5ea897964c0a0a9816a8b7cf3ac
iverty <berezhnoy20025@gmail.com>
2022-07-03 19:08:55 +0300

    view train template fixed

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -2,4 +2,7 @@ class Wagon < ApplicationRecord
   belongs_to :train, optional: true
 
   validates :number, :top_seats, :bottom_seats, presence: true
+
+  scope :coupe_wagons, -> { where(type: 'CoupeWagon') }
+  scope :economy_wagons, -> { where(type: 'EconomyWagon') }
 end
